fd3169d0acbceb24602e83a58eed8cbe5d2c851c
AynazFlex <87998138+AynazFlex@users.noreply.github.com>
2021-10-28 21:13:15 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ document.addEventListener('pointerdown', function(event) {
       console.log(event.clientX - x);
       if(event.clientX - x < -5) {
         elem.style.left = -62 + 'px';
-        setTimeout(() => elem.remove(), 600);
+        setTimeout(() => elem.remove(), 500);
       }
       if(event.clientX - x > 5) {
         elem.style.left = 62 + 'px';
